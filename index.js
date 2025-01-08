b1e5c78308c1afa08a6a84d45deeaaad7232e208
cperez7733@gmail.com <techgirlalchemy@gmail.com>
2025-01-08 12:02:59 -0500

    added express

--- index.js
+++ index.js
@@ -1,54 +1,48 @@
-var _ = require("underscore");
+const express = require("express");
+const app = express();
 
-const movies = [
-  {
-    title: "The Galactic Voyage",
-    runtime: 142,
-    rating: 8.5,
-    year: 2021,
-  },
-  {
-    title: "Mystic Shadows",
-    runtime: 98,
-    rating: 7.2,
-    year: 2019,
-  },
-  {
-    title: "Rogue Element",
-    runtime: 115,
-    rating: 8.1,
-    year: 2023,
-  },
-  {
-    title: "Timeless Quest",
-    runtime: 130,
-    rating: 9.0,
-    year: 2020,
-  },
-  {
-    title: "Echoes of Eternity",
-    runtime: 124,
-    rating: 8.7,
-    year: 2022,
-  },
-  {
-    title: "Parallel Realities",
-    runtime: 107,
-    rating: 7.8,
-    year: 2018,
-  },
-  {
-    title: "Legacy of the Titans",
-    runtime: 150,
-    rating: 9.2,
-    year: 2021,
-  },
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+const courses = [
+  { id: 1, name: "chess" },
+  { id: 2, name: "geography" },
+  { id: 3, name: "math" },
 ];
 
-var results = [];
-
-_.forEach(movies, function (movie) {
-  results.push(movie.title);
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
-console.log(results);
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+//api/courses/1
+app.get("/api/courses/:id", (req, res) => {
+  const courseID = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!courseID) {
+    res.status(404).send("Course with the ID provided is not found");
+  }
+  res.send(
+    `Great news, we found what you were looking for the class is - ${courseID.name}`
+  );
+});
+
+//post a course
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+
+  res.send(course);
+});
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
+//test
